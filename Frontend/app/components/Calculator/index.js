7891875bbbeabf9d76849d78b944aa00d55c6fc7
Hussain-M53 <hash.hussain53@gmail.com>
2023-10-15 10:22:56 +0500

    bug fixed

--- Frontend/app/components/Calculator/index.js
+++ Frontend/app/components/Calculator/index.js
@@ -52,6 +52,10 @@ const Calculator = () => {
 
   const handleFormChange = (e) => {
     const { name, value } = e.target;
+    updateFormData(name, value);
+  };
+
+  const updateFormData = (name, value) => {
     setOrderDetails((prevData) => ({
       ...prevData,
       [name]: value
@@ -216,7 +220,7 @@ const Calculator = () => {
               onChange={handleFormChange}
               className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
             >
-               <option disabled selected style={{ display: 'none' }}>Subject</option>
+              <option disabled selected style={{ display: 'none' }}>Subject</option>
               {getSubjectOptions().map((subj, idx) => <option key={idx}>{subj}</option>)}
             </select>
           </div>
